d259fc0536ac3151d184fde4845deababd00c27d
JIXON721 <gmc1642@gmail.com>
2026-03-14 14:06:06 -0700

    Update 24_tryCatch.dart

--- 24_tryCatch.dart
+++ 24_tryCatch.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 int factorial(int n) {
   if (n < 0) {
-    throw Exception("El factorial no existe para numeros negativos");
+    throw Exception("Factorial does not exist for negative numbers.");
   }
   if (n == 0 || n == 1) {
     return 1;
@@ -11,15 +11,14 @@ int factorial(int n) {
 }
 
 void main() {
-  print("Ingrese un numero:");
-
+  print("Enter a number:");
   try {
     String? input = stdin.readLineSync();
-    int numero = int.parse(input!);
-    int resultado = factorial(numero);
-    print("El factorial de $numero es: $resultado");
+    int number = int.parse(input!);
+    int result = factorial(number);
+    print("The factorial of $number is: $result");
   } on FormatException {
-    print("Ingrese un numero valido.");
+    print("Please enter a valid number.");
   } catch (e) {
     print(e);
   }
